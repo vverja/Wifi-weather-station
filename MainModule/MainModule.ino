fec2df61174795bf7a36bc4c318266479c7c4a92
FromWork <theodor.voznesenski@gmail.com>
2018-02-22 08:57:05 +0200

    Working scetch

--- MainModule/MainModule.ino
+++ MainModule/MainModule.ino
@@ -5,7 +5,16 @@
  *  below. Or just customize this script to talk to other HTTP servers.
  *
  */
+ 
  #include <ESP8266WiFi.h>
+
+   
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  
+ #include <ArduinoJson.h>
+ const char *filename = "/config.txt";  // <- SD library uses 8.3 filenames
+
  
  /*инициализация сд карты*/
 #include <SPI.h>
@@ -20,36 +29,37 @@ RtcDS1307<TwoWire> Rtc(Wire);
 
 //Библиотеки для работы с температурным датчиком
 #include "DHT.h"
-#define DHTPIN 2
+#define DHTPIN 4 //D2
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
 //Библиотеки для работы с экраном
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27,20,4);
+LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
 
 
 File DataFile;
-File IniFile;
+//File IniFile;
 
 struct iniStruct{
-  char* ssid;
-  char* password;
-  char* host; 
+  char ssid[64];
+  char password[100];
+  char host[64];
+  char url[255]; 
   int port;
 };
-struct iniStruct ini ;
+iniStruct config ;
 
 void setup() {
   Serial.begin(115200);
   delay(10);
 
-  
-  lcd.begin(0,2);
-  lcd.backlight();
+  lcd.begin(16, 2);
+  lcd.clear();
 
   
   //инициализируем время
+   Wire.begin(0, 2);
    Rtc.Begin();
 
     RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
@@ -58,13 +68,13 @@ void setup() {
     if (!Rtc.IsDateTimeValid()) 
     {
       //если не чип инициализируется после потери питания
-        lcd.print("I lost the DateTime!");
+        Serial.print("I lost the DateTime!");
         Rtc.SetDateTime(compiled);
     }
 
     if (!Rtc.GetIsRunning())
     {
-        lcd.println("Time is starting now");
+        Serial.println("Time is starting now");
         Rtc.SetIsRunning(true);
     }
 
@@ -89,7 +99,8 @@ void setup() {
   Serial.println("initialization done.");
 
 
-  readIniFile(&ini);
+  //readIniFile(&ini);
+  loadConfiguration(filename, config);
   
   DHT dht(DHTPIN, DHTTYPE);  
 
@@ -98,17 +109,30 @@ void setup() {
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
-  Serial.println(ini.ssid);
+  Serial.println(config.ssid);
   
   /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
      would try to act as both a client and an access-point and could cause
      network-issues with your other WiFi-devices on your WiFi-network. */
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ini.ssid, ini.password);
-  
+  WiFi.begin(config.ssid, config.password);
+  int count = 0; 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(1000);
     Serial.print(".");
+    if (count > 15){
+      WiFi.begin("finance", "kvadrat1");
+      Serial.print("\nConnecting to ");
+      Serial.println("finance");
+      while (WiFi.status() != WL_CONNECTED) {
+          delay(1000);
+          Serial.print(".");    
+          if (count > 15){
+            ESP.reset();  
+          }
+        }    
+    }
+    count++;
   }
 
   Serial.println("");
@@ -120,8 +144,7 @@ void setup() {
 int value = 0;
 
 void loop() {
-  delay(5000);
-  ++value;
+
 
   if (!Rtc.IsDateTimeValid()) 
   {
@@ -131,22 +154,18 @@ void loop() {
   }
 
   RtcDateTime now = Rtc.GetDateTime();
-
-  char* currentDate = printDateTime(now, false);
- 
+  char currentDate[20]; 
+  printDateTime(currentDate, now, false);
+  Serial.println(currentDate);
+  
+  char currentDateTime[20]; 
+  printDateTime(currentDateTime, now, true);
+  Serial.println(currentDateTime);
+  
   Serial.print("connecting to ");
-  Serial.println(ini.host);
-  
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  //const int httpPort = 80;
-  
-  if (!client.connect(ini.host, ini.port)) {
-    Serial.println("connection failed");
-    return;
-  }
-  
-  
+  Serial.println(config.host);
+
+   
   float h = dht.readHumidity();
   
   float t = dht.readTemperature();
@@ -156,109 +175,147 @@ void loop() {
     return;
   }
 
-  writeToSD(h, t, currentDate);
+  writeToSD(h, t, currentDate, currentDateTime);
   
-//  Serial.print("Requesting URL: ");
-//  Serial.println(url);
-  
-  // This will send the request to the server
-//  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-//               "Host: " + host + "\r\n" + 
-//               "Connection: close\r\n\r\n");
-//  unsigned long timeout = millis();
-//  while (client.available() == 0) {
-//    if (millis() - timeout > 5000) {
-//      Serial.println(">>> Client Timeout !");
-//      client.stop();
-//      return;
-//    }
-//  }
-  
-  // Read all the lines of the reply from server and print them to Serial
-//  while(client.available()){
-//    String line = client.readStringUntil('\r');
-//    Serial.print(line);
-//  }
-  
-  Serial.println();
-  Serial.println("closing connection");
+  if(!postPage(config.host,config.port,"/deviceController/setDataFromWifiDevices",getJsonString(t, h))) Serial.print(F("Fail "));
+    else Serial.print(F("Pass "));
+  delay(3600000);
 }
 
-void readIniFile(struct iniStruct *ini){
-  String property, value, temp;
-  
-  
-  IniFile = SD.open("sensor.ini");
-  if (IniFile)
-    while (IniFile.available()) {
-    //while (fscanf (IniFile, "%s%s", property,value) != EOF) {
-     char thisChar = IniFile.read();
-     temp.concat(thisChar);
-     if(thisChar == ':'){
-        property = temp;
-        temp = "";   
-      }
-      if(thisChar == '\n'){
-        value = temp;
-        temp = "";  
-      }
-      if(property == "host:")
-        value.toCharArray(ini->host,value.length());
-      else if(property == "ssid:")
-        value.toCharArray(ini->ssid,value.length());
-      else if(property == "password:")
-        value.toCharArray(ini->password,value.length());
-      else if(property  == "port:")
-        ini->port = value.toInt();
-      property= "";
-      value = "";
-    }
-     
+
+void loadConfiguration(const char *filename, iniStruct &config) {
+  // Open file for reading
+  File file = SD.open(filename);
+
+  // Allocate the memory pool on the stack.
+  // Don't forget to change the capacity to match your JSON document.
+  // Use arduinojson.org/assistant to compute the capacity.
+  StaticJsonBuffer<512> jsonBuffer;
+
+  // Parse the root object
+  JsonObject &root = jsonBuffer.parseObject(file);
+
+  if (!root.success())
+    Serial.println(F("Failed to read file"));
+    
+  // Copy values from the JsonObject to the Config
+  strlcpy(config.ssid, root["ssid"],sizeof(config.ssid));
+  strlcpy(config.host, root["host"], sizeof(config.host));
+  strlcpy(config.password, root["password"], sizeof(config.password));
+  strlcpy(config.url, root["url"], sizeof(config.url));
+  config.port = root["port"];
+  // Close the file (File's destructor doesn't close the file)
+  file.close();
 }
 
-void writeToSD(float h, float t, char* currentDate){
-  String fileName = String(currentDate)+".txt";
+void writeToSD(float h, float t, char currentDate[], char currentDateTime[]){
+  String fileName = String(currentDate);
+  fileName.trim();
+  fileName = fileName + ".txt";
+  Serial.println("Trying to write into the file " + fileName);
   File myFile = SD.open(fileName, FILE_WRITE);
-
+  
   // if the file opened okay, write to it:
   if (myFile) {
-    myFile.print("Humidity: ");
+    myFile.print(">>> ");
+    myFile.print(currentDateTime);
+    myFile.print("H: ");
     myFile.print(h);
     myFile.print(" %\t");
-    myFile.print("Temperature: ");
+    myFile.print("T: ");
     myFile.print(t);
     myFile.print(" *C ");
-    
+    myFile.print("\n");
     myFile.close();
 
   } else {
     // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
+    Serial.println("error opening " + fileName);
   }  
 }
 
 #define countof(a) (sizeof(a) / sizeof(a[0]))
 
-char* printDateTime(const RtcDateTime& dt, bool type)
+void printDateTime(char datestring[], const RtcDateTime& dt, bool type)
 {
-    char datestring[20];
   if(type)
     snprintf_P(datestring, 
-            countof(datestring),
+            20,
             PSTR("%02u/%02u/%04u %02u:%02u"),
-            dt.Month(),
             dt.Day(),
+            dt.Month(),
             dt.Year(),
             dt.Hour(),
             dt.Minute());
   else{
         snprintf_P(datestring, 
-            countof(datestring),
-            PSTR("%02u_%02u_%04u"),
-            dt.Month(),
+            20,
+            PSTR("%02u%02u%04u"),
             dt.Day(),
+            dt.Month(),
             dt.Year());    
     } 
-    return datestring;
 }
+
+byte postPage(char* domainBuffer,int thisPort,char* page,String thisData)
+{
+  int inChar;
+  char outBuf[64];
+  Serial.println(thisData);
+  Serial.print(F("connecting..."));
+
+  if(client.connect(domainBuffer,thisPort) == 1)
+  {
+    Serial.println(F("connected"));
+
+    // send the header
+    sprintf(outBuf,"POST %s HTTP/1.1",page);
+    client.println(outBuf);
+    sprintf(outBuf,"Host: %s",domainBuffer);
+    client.println(outBuf);
+    client.println(F("Connection: close\r\nContent-Type: application/x-www-form-urlencoded"));
+    sprintf(outBuf,"Content-Length: %u\r\n",(thisData.length()));
+    client.println(outBuf);
+
+    // send the body (variables)
+    client.print(thisData);
+  } 
+  else
+  {
+    Serial.println(F("failed"));
+    return 0;
+  }
+
+  int connectLoop = 0;
+
+  while(client.connected())
+  {
+    while(client.available())
+    {
+      inChar = client.read();
+      Serial.write(inChar);
+      connectLoop = 0;
+    }
+
+    delay(1);
+    connectLoop++;
+    if(connectLoop > 10000)
+    {
+      Serial.println();
+      Serial.println(F("Timeout"));
+      client.stop();
+    }
+  }
+
+  Serial.println();
+  Serial.println(F("disconnecting."));
+  client.stop();
+  return 1;
+}
+String getJsonString(float t, float h){
+    //sprintf(outBuf,"{\'obj\':\'{\'id\':\'1\', \'type\':\'W\', \'temp\':\'%u\', \'hum\':\'%u\'}\'}",t,h);
+    
+    return "obj={\"id\":1, \"type\":\"W\", \"temp\":" + String(t) + ", \"hum\":" + String(h) + ", \"name\":\"Device1\"}";
+    
+  }
 
